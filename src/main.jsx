192916ac46514e286f884adeade36a2a2fc1a471
Fred-temp-77 <fklatigogo.777@gmail.com>
2026-09-24 11:23:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Example NotFound page
+const NotFound = () => <h1>Page Not Found</h1>
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+])
+
+// Rendering to DOM
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
